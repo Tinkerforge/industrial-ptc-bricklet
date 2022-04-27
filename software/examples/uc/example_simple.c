a9baf82734a4d54730f62f7bcad2f371af04c14e
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:34:57 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_industrial_ptc.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_industrial_ptc.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Industrial PTC Bricklet
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_IndustrialPTC ptc;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_industrial_ptc_create(&ptc, UID, hal), "create device object");
+	check(tf_industrial_ptc_create(&ptc, NULL, hal), "create device object");
 
 	// Get current temperature
 	int32_t temperature;
